e1160a47382d3342d1a923245dead6915d29b873
Andrian Budantsov <andrian@readdle.com>
2016-08-22 18:57:20 +0300

    - more install script updates

--- install_systemd.sh
+++ install_systemd.sh
@@ -3,7 +3,7 @@ set -e
 
 NAME=memdisk-cloudwatch
 VERSION=v0.9.2
-BIN_URL=https://github.com/AndrianBdn/memdisk-cloudwatch/releases/download/$VERSION/memdisk-cloudwatch-x86_64.gz
+BIN_URL=https://github.com/AndrianBdn/$NAME/releases/download/$VERSION/$NAME-x86_64.gz
 BIN_PATH=/usr/local/bin/$NAME
 TMP_BIN=/tmp/$NAME
 SYSTEMD_UNIT=/etc/systemd/system/$NAME.service
@@ -14,7 +14,7 @@ chmod 755 $TMP_BIN
 set +e
 if ! $TMP_BIN -runonce 1
 then
-    echo "got error when trying memdisk-cloudwatch"
+    echo "got error when trying $NAME"
     echo "not continuing to install"
     exit 1
 fi
@@ -47,4 +47,4 @@ systemctl enable $NAME
 systemctl start $NAME
 fi
 
-touch /root/.memdisk-cloudwatch-$VERSION
+touch /root/.$NAME-$VERSION
--- install_sysv.sh
+++ install_sysv.sh
@@ -3,7 +3,7 @@ set -e
 
 NAME=memdisk-cloudwatch
 VERSION=v0.9.2
-BIN_URL=https://github.com/AndrianBdn/memdisk-cloudwatch/releases/download/$VERSION/memdisk-cloudwatch-x86_64.gz
+BIN_URL=https://github.com/AndrianBdn/$NAME/releases/download/$VERSION/$NAME-x86_64.gz
 BIN_PATH=/usr/local/bin/$NAME
 TMP_BIN=/tmp/$NAME
 
@@ -13,7 +13,7 @@ chmod 755 $TMP_BIN
 set +e
 if ! $TMP_BIN -runonce 1
 then
-    echo "got error when trying memdisk-cloudwatch"
+    echo "got error when trying $NAME"
     echo "not continuing to install"
     exit 1
 fi
@@ -22,12 +22,12 @@ set -e
 rm -f $BIN_PATH
 cp $TMP_BIN $BIN_PATH
 
-TMPCRON=$(mktemp /tmp/memdisk-cw-cron.XXXXXX)
+TMPCRON=$(mktemp /tmp/$NAME-cron.XXXXXX)
 crontab -l | grep -v $NAME > $TMPCRON || true 
 
 echo "*/5 * * * * /usr/local/bin/$NAME -crontab 1" >> $TMPCRON
 cat $TMPCRON | crontab - 
 rm -f $TMPCRON
 
-touch /root/.memdisk-cloudwatch-$VERSION
+touch /root/.$NAME-$VERSION
 
